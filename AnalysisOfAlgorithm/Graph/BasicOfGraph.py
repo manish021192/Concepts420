f0d35b13904b5d61805ef13fe73075f6ecf6af91
manish021192 <manishpatwalmailbox@gmail.com>
2024-08-20 16:32:18 +0530

    added Graph folder

---
+++ AnalysisOfAlgorithm/Graph/BasicOfGraph.py
@@ -0,0 +1,18 @@
+"""
+A Graph is a data structure that consists of the following two components:
+
+    A finite set of vertices also called nodes.
+    A finite set of ordered pair of the form (u, v) called as edge
+
+    Real life example : Google GPS , Facebook ,Linkedin
+
+Directed Graphs: The Directed graphs are such graphs in which edges are directed in a single
+direction.
+Undirected Graphs : Undirected graphs are such graphs in which the edges are directionless
+or in other words bi-directional.
+
+
+Following two are the most commonly used representations of a graph:
+    1) Adjacency Matrix.
+    2) Adjacency List.
+"""
